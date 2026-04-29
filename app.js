3e747ac61a1d7d078e91b0c2de94ead1eae030ce
Mice <180280929+MiceDe27@users.noreply.github.com>
2026-04-29 16:58:30 +0800

    Initial commit

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(cors({
     origin: [
-        process.env.FRONTEND_URL || 'http://localhost:5173', 
+        'http://localhost:5173', 
         'capacitor://localhost', 
         'http://localhost'
     ],
