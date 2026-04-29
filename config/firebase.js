b93bcb4ec1b4271c2bd66fa6650eb35f979e142a
Mice <180280929+MiceDe27@users.noreply.github.com>
2026-04-29 12:06:37 +0800

    Initial commit

--- config/firebase.js
+++ config/firebase.js
@@ -1,9 +1,16 @@
 var admin = require("firebase-admin");
-var serviceAccount = require("./firebase-service-key.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://ams-project-8fd8a-default-rtdb.firebaseio.com"
-});
+if (!admin.apps.length) {
+  try {
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL: "https://ams-project-8fd8a-default-rtdb.firebaseio.com"
+    });
+  } catch (error) {
+    console.error("Firebase Init Error:", error.message);
+  }
+}
 
 module.exports = admin;
