fce60e0d4531742f361f4de57b22e1bbb72f848a
Mice <180280929+MiceDe27@users.noreply.github.com>
2026-04-29 16:29:56 +0800

    Initial commit

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ app.use(cors({
     origin: [
         process.env.FRONTEND_URL || 'http://localhost:5173', 
         'capacitor://localhost', 
-        'http://localhost'
+        'http://localhost',
+        '*'
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'x-tenant-id'],
